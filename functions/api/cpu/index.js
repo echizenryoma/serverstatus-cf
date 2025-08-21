47ca721de888c59c9fdb2c4a0d5d00bb4b00d945
Ryoma Dev <dev@rdbg.net>
2025-08-22 07:22:33 +0800

    functions: add cpu

---
+++ functions/api/cpu/index.js
@@ -0,0 +1,37 @@
+export async function onRequest({ request, env }) {
+  const influxdb_host = env.INFLUXDB_HOST;
+  const influxdb_token = env.INFLUXDB_TOKEN;
+  const influxdb_org = env.INFLUXDB_ORG;
+
+  if (request.method === 'GET') {
+    try {
+      const query_url = new URL(`https://${influxdb_host}/api/v2/query?org=${influxdb_org}&t=${Date.now()}`);
+      const influx_ql = `
+from(bucket: "server")
+  |> range(start: -1m)
+  |> filter(fn: (r) => r["_measurement"] == "cpu")
+  |> filter(fn: (r) => r["_field"] == "usage_steal" or r["_field"] == "usage_system" or r["_field"] == "usage_user")
+  |> filter(fn: (r) => r["cpu"] == "cpu-total")
+  |> last(column: "host")
+  |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+  |> keep(columns: ["_time", "host", "usage_steal", "usage_system", "usage_user"])
+`
+      const response = await fetch(query_url, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Token ${influxdb_token}`,
+          'Content-Type': 'application/vnd.flux',
+        },
+        body: influx_ql,
+      });
+      if (!response.ok) throw new Error(`fetch CSV failed: ${res.status} ${res.statusText}`);
+      const csvText = await response.text();
+      return new Response(csvText, {
+        headers: { "content-type": "text/csv" },
+      });
+    } catch (error) {
+      return new Response('Internal Server Error', { status: 500 });
+    }
+  }
+  return new Response('Method Not Allowed', { status: 405 });
+}
