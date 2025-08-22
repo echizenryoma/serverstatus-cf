7cc76463af7f88af50b31e005a7ee3ef7da5e460
Ryoma Dev <dev@rdbg.net>
2025-08-22 20:21:15 +0800

    ui: dynamicTyping

--- src/components/ServerStatus.js
+++ src/components/ServerStatus.js
@@ -69,28 +69,28 @@ export default {
     updateViewData() {
       this.viewData = this.db.map(item => ({
         host: item.host,
-        uptime: this.formatSeconds(parseInt(item.cpu.uptime)),
+        uptime: this.formatSeconds(item.cpu.uptime),
         net_proto: `${this.getNetProtoFlag(item.info.have_ipv4)}|${this.getNetProtoFlag(item.info.have_ipv6)}`,
         location: getFlagEmoji(item.info.loc),
-        cpu: Math.round(parseFloat(item.cpu.usage_user) + parseFloat(item.cpu.usage_system)),
-        memory: Math.round(((parseInt(item.mem.used) || 0) / (parseInt(item.mem.total)) || 0) * 100),
-        disk: Math.round(((parseInt(item.disk.used) || 0) / (parseInt(item.disk.total)) || 0) * 100),
-        load: (parseFloat(item.cpu.load1) || 0.0).toFixed(2),
-        net: `${fileSizePretty(parseInt(item.net.bytes_recv) || 0)}|${fileSizePretty(parseInt(item.net.bytes_sent) || 0)}`,
-        traffic: `${fileSizePretty(parseInt(item.traffic.bytes_recv) || 0)}|${fileSizePretty(parseInt(item.traffic.bytes_sent) || 0)}`,
-        load_detail: `${(parseFloat(item.cpu.load1) || 0.0).toFixed(2)} / ${(parseFloat(item.cpu.load5) || 0.0).toFixed(2)} / ${(parseFloat(item.cpu.load15) || 0.0).toFixed(2)}`,
-        cpu_detail: `${(parseFloat(item.cpu.usage_system) || 0.0).toFixed(2)}% / ${(parseFloat(item.cpu.usage_user) || 0.0).toFixed(2)}% / ${(parseFloat(item.cpu.usage_steal) || 0.0).toFixed(2)}%`,
-        memory_detail: `${fileSizePretty(parseInt(item.mem.used) || 0)} / ${fileSizePretty(parseInt(item.mem.total) || 0)}`,
-        swap_detail: `${fileSizePretty(parseInt(item.mem.swap_cached) || 0)} / ${fileSizePretty(parseInt(item.mem.swap_total) || 0)}`,
-        disk_detail: `${fileSizePretty(parseInt(item.disk.used) || 0)} / ${fileSizePretty(parseInt(item.disk.total) || 0)}`,
-        network_detail: `${parseInt(item.info.down_mbps) || 0} Mbps / ${parseInt(item.info.up_mbps) || 0}Mbps `,
-        loss_cm: item.info.have_ipv4 === 'yes' ? (parseFloat(item.ping.loss_cmv4) || 0.0).toFixed(0) : (parseFloat(item.ping.loss_cmv6) || 0.0).toFixed(0),
-        loss_ct: item.info.have_ipv4 === 'yes' ? (parseFloat(item.ping.loss_ctv4) || 0.0).toFixed(0) : (parseFloat(item.ping.loss_ctv6) || 0.0).toFixed(0),
-        loss_cu: item.info.have_ipv4 === 'yes' ? (parseFloat(item.ping.loss_cuv4) || 0.0).toFixed(0) : (parseFloat(item.ping.loss_cuv6) || 0.0).toFixed(0),
-        lossv4_detail: `${(parseFloat(item.ping.loss_cmv4) || 0.0).toFixed(0)}% / ${(parseFloat(item.ping.loss_ctv4) || 0.0).toFixed(0)}% / ${(parseFloat(item.ping.loss_cuv4) || 0.0).toFixed(0)}%`,
-        pingv4_detail: `${(parseFloat(item.ping.ping_cmv4) || 0.0).toFixed(0)}ms / ${(parseFloat(item.ping.ping_ctv4) || 0.0).toFixed(0)}ms / ${(parseFloat(item.ping.ping_cuv4) || 0.0).toFixed(0)}ms`,
-        lossv6_detail: `${(parseFloat(item.ping.loss_cmv6) || 0.0).toFixed(0)}% / ${(parseFloat(item.ping.loss_ctv6) || 0.0).toFixed(0)}% / ${(parseFloat(item.ping.loss_cuv6) || 0.0).toFixed(0)}%`,
-        pingv6_detail: `${(parseFloat(item.ping.ping_cmv6) || 0.0).toFixed(0)}ms / ${(parseFloat(item.ping.ping_ctv6) || 0.0).toFixed(0)}ms / ${(parseFloat(item.ping.ping_cuv6) || 0.0).toFixed(0)}ms`
+        cpu: Math.round(item.cpu.usage_user + item.cpu.usage_system),
+        memory: Math.round(item.mem.used / item.mem.total * 100),
+        disk: Math.round(item.disk.used / item.disk.total * 100),
+        load: item.cpu.load1.toFixed(2),
+        net: `${fileSizePretty(item.net.bytes_recv)}|${fileSizePretty(item.net.bytes_sent)}`,
+        traffic: `${fileSizePretty(item.traffic.bytes_recv)}|${fileSizePretty(item.traffic.bytes_sent)}`,
+        load_detail: `${item.cpu.load1.toFixed(2)} / ${item.cpu.load5.toFixed(2)} / ${item.cpu.load15.toFixed(2)}`,
+        cpu_detail: `${item.cpu.usage_system.toFixed(2)}% / ${item.cpu.usage_user.toFixed(2)}% / ${item.cpu.usage_steal.toFixed(2)}%`,
+        memory_detail: `${fileSizePretty(item.mem.used)} / ${fileSizePretty(item.mem.total)}`,
+        swap_detail: `${fileSizePretty(item.mem.swap_cached)} / ${fileSizePretty(item.mem.swap_total)}`,
+        disk_detail: `${fileSizePretty(item.disk.used)} / ${fileSizePretty(item.disk.total)}`,
+        network_detail: `${item.info.down_mbps} Mbps / ${item.info.up_mbps}Mbps `,
+        loss_cm: item.info.have_ipv4 === 'yes' ? item.ping.loss_cmv4.toFixed(2) : item.ping.loss_cmv6.toFixed(2),
+        loss_ct: item.info.have_ipv4 === 'yes' ? item.ping.loss_ctv4.toFixed(2) : item.ping.loss_ctv6.toFixed(2),
+        loss_cu: item.info.have_ipv4 === 'yes' ? item.ping.loss_cuv4.toFixed(2) : item.ping.loss_cuv6.toFixed(2),
+        lossv4_detail: `${item.ping.loss_cmv4.toFixed(2)}% / ${item.ping.loss_ctv4.toFixed(2)}% / ${item.ping.loss_cuv4.toFixed(2)}%`,
+        pingv4_detail: `${item.ping.ping_cmv4.toFixed(2)}ms / ${item.ping.ping_ctv4.toFixed(2)}ms / ${item.ping.ping_cuv4.toFixed(2)}ms`,
+        lossv6_detail: `${item.ping.loss_cmv6.toFixed(2)}% / ${item.ping.loss_ctv6.toFixed(2)}% / ${item.ping.loss_cuv6.toFixed(2)}%`,
+        pingv6_detail: `${item.ping.ping_cmv6.toFixed(2)}ms / ${item.ping.ping_ctv6.toFixed(2)}ms / ${item.ping.ping_cuv6.toFixed(2)}ms`
       }));
     },
     async fetchData() {
@@ -124,7 +124,7 @@ export default {
             return;
           }
           const csvText = res.data;
-          const parsed = Papa.parse(csvText, { header: true });
+          const parsed = Papa.parse(csvText, { header: true, dynamicTyping: true });
           parsed.data.filter(
             (row) => row.host && row.host.trim() !== ""
           ).forEach((row) => {
