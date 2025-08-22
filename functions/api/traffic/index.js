3758715e629e2d64fc3a685123bf777d7ca1e403
Ryoma Dev <dev@rdbg.net>
2025-08-22 10:03:56 +0800

    functions: add traffic

---
+++ functions/api/traffic/index.js
@@ -0,0 +1,38 @@
+export async function onRequest({ request, env }) {
+  const influxdb_host = env.INFLUXDB_HOST;
+  const influxdb_token = env.INFLUXDB_TOKEN;
+  const influxdb_org = env.INFLUXDB_ORG;
+
+  if (request.method === 'GET') {
+    try {
+      const query_url = new URL(`https://${influxdb_host}/api/v2/query?org=${influxdb_org}&t=${Date.now()}`);
+      const influx_ql = `
+from(bucket: "server")
+  |> range(start: -5s)
+  |> filter(fn: (r) => r["_measurement"] == "net")
+  |> filter(fn: (r) => r["_field"] == "bytes_recv" or r["_field"] == "bytes_sent")
+  |> last()
+  |> group(columns: ["_field", "_time", "host"])
+  |> sum()
+  |> pivot(rowKey:["_time", "host"], columnKey: ["_field"], valueColumn: "_value")
+  |> keep(columns: ["_time", "host", "bytes_recv", "bytes_sent"])
+`
+      const response = await fetch(query_url, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Token ${influxdb_token}`,
+          'Content-Type': 'application/vnd.flux',
+        },
+        body: influx_ql,
+      });
+      if (!response.ok) throw new Error(`fetch CSV failed: ${res.status} ${res.statusText}`);
+      const csvText = await response.text();
+      return new Response(csvText, {
+        headers: { "content-type": "text/csv" },
+      });
+    } catch (error) {
+      return new Response('Internal Server Error', { status: 500 });
+    }
+  }
+  return new Response('Method Not Allowed', { status: 405 });
+}
