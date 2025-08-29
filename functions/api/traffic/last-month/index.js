8f558ae53645ee2d4fef04d4c491ddd8c2f443dc
Ryoma Dev <dev@rdbg.net>
2025-08-29 22:19:45 +0800

    functions: add api/traffic/last-month

---
+++ functions/api/traffic/last-month/index.js
@@ -0,0 +1,59 @@
+export async function onRequest({ request, env }) {
+  const influxdb_host = env.INFLUXDB_HOST;
+  const influxdb_token = env.INFLUXDB_TOKEN;
+  const influxdb_org = env.INFLUXDB_ORG;
+  const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  };
+
+  if (request.method === 'GET') {
+    try {
+      const now = new Date();
+
+      let stop = new Date(now);
+      stop.setUTCHours(0, 5, 0, 0);
+      stop.setUTCDate(1);
+      if (now < stop) {
+        stop.setUTCMonth(stop.getUTCMonth() - 1);
+      }
+      const start = new Date(stop);
+      start.setUTCHours(0, 0, 0, 0);
+
+      console.log("start: ", start.toISOString());
+      console.log("stop: ", stop.toISOString());
+
+      const query_url = new URL(`https://${influxdb_host}/api/v2/query?org=${influxdb_org}`);
+      const influx_ql = `
+from(bucket: "history")
+  |> range(start: ${start.toISOString()}, stop: ${stop.toISOString()})
+  |> filter(fn: (r) => r["_measurement"] == "net")
+  |> filter(fn: (r) => r["_field"] == "bytes_recv" or r["_field"] == "bytes_sent")
+  |> last()
+  |> group(columns: ["_field", "host"])
+  |> sum()
+  |> pivot(rowKey:["host"], columnKey: ["_field"], valueColumn: "_value")
+  |> keep(columns: ["host", "bytes_recv", "bytes_sent"])
+`
+      const response = await fetch(query_url, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Token ${influxdb_token}`,
+          'Content-Type': 'application/vnd.flux',
+        },
+        body: influx_ql,
+      });
+      if (!response.ok) throw new Error(`fetch CSV failed: ${response.status} ${response.statusText}`);
+      const csvText = await response.text();
+      const headers = corsHeaders;
+      headers['Content-Type'] = 'text/csv';
+      return new Response(csvText, {
+        headers: headers,
+      });
+    } catch (error) {
+      return new Response('Internal Server Error', { status: 500 });
+    }
+  }
+  return new Response('Method Not Allowed', { status: 405 });
+}
