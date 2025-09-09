51647400e19180a7b33b59f3c6b2cf0b86cbd2a6
Ryoma Dev <dev@rdbg.net>
2025-09-09 21:21:03 +0800

    ui: add zhHant

--- src/config/languageOptions.js
+++ src/config/languageOptions.js
@@ -1,4 +1,5 @@
 export default [
   { text: '简体中文', value: 'zhHans' },
+  { text: '繁体中文', value: 'zhHant' },
   { text: 'English', value: 'en' }
 ];
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,8 +1,9 @@
 import { createI18n } from 'vue-i18n'
 import en from './en.json'
 import zhHans from './zhHans.json'
+import zhHant from './zhHant.json'
 
-import { en as vuetifyEn, zhHans as vuetifyZhHans } from 'vuetify/locale'
+import { en as vuetifyEn, zhHans as vuetifyZhHans, zhHant as vuetifyZhHant } from 'vuetify/locale'
 
 export const messages = {
   en: {
@@ -15,6 +16,12 @@ export const messages = {
     ...zhHans,
     $vuetify: {
       ...vuetifyZhHans,
+    },
+  },
+  zhHant: {
+    ...zhHant,
+    $vuetify: {
+      ...vuetifyZhHant,
     },
   },
 };
