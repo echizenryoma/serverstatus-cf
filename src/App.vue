c0e427f2a66b9ad6e26bbb38157b27bda5b4b35e
Ryoma Dev <dev@rdbg.net>
2025-11-12 20:49:57 +0800

    ui: add background

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
   <v-app>
     <v-main>
-      <router-view />
+      <div class="global-bg">
+        <router-view />
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-  //
+//
 </script>
+
+<style>
+.global-bg {
+  background-image: url('@/assets/background.jpg');
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center;
+  min-height: 100vh;
+  z-index: -1;
+}
+</style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,27 +1,30 @@
 <template>
-  <v-footer app absolute color="transparent" class="text-center d-flex flex-column">
-    <v-col class="text-caption">
-      <i18n-t keypath="app.footer.poweredBy">
-        <template #influxDb>
-          <a href="https://www.influxdata.com/" target="_blank">InfluxDB</a>
-        </template>
-        <template #cloudflarePage>
-          <a href="https://pages.cloudflare.com/" target="_blank">Cloudflare Pages</a>
-        </template>
-        <template #vuetify>
-          <a href="https://vuetifyjs.com/" target="_blank">Vuetify</a>
-        </template>
-      </i18n-t>
-    </v-col>
-    <v-col class="text-caption">
-      <i18n-t keypath="app.footer.copyright">
-        <template #year>{{ new Date().getFullYear() }}</template>
-        <template #author>Ryoma Dev</template>
-      </i18n-t>
-    </v-col>
-    <v-col class="text-caption">
-      <i18n-t keypath="app.footer.version"></i18n-t>: {{ appVersion }}
-    </v-col>
+  <v-footer app absolute color="transparent" class="text-center flex-column">
+      <v-col class="text-caption">
+        {{ appVersion }}
+        <i18n-t keypath="app.footer.poweredBy">
+          <template #influxDb>
+            <a href="https://www.influxdata.com/" target="_blank">InfluxDB</a>
+          </template>
+          <template #cloudflarePage>
+            <a href="https://pages.cloudflare.com/" target="_blank">Cloudflare Pages</a>
+          </template>
+          <template #vuetify>
+            <a href="https://vuetifyjs.com/" target="_blank">Vuetify</a>
+          </template>
+        </i18n-t>
+      </v-col>
+      <v-col  class="text-caption">
+        <i18n-t keypath="app.footer.copyright">
+          <template #year>{{ new Date().getFullYear() }}</template>
+          <template #author>Ryoma Dev</template>
+        </i18n-t>
+      </v-col>
+      <v-col class="d-flex justify-center">
+        <v-avatar size="42">
+          <v-img src="@/assets/author.png" alt="作者"></v-img>
+        </v-avatar>
+      </v-col>
   </v-footer>
 </template>
 
